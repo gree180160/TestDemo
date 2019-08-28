fc94d58aa3f09d6149f078611dd1f3db492a4ecf
gree180160 <1459287460@qq.com>
2019-08-29 02:08:49 +0800

    2.ti jiao

---
+++ SDKDemo/SDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDKDemo
+//
+//  Created by Willow Rivers on 2019/8/29.
+//  Copyright © 2019 Willow Rivers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDKDemo/SDKDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SDKDemo
+//
+//  Created by Willow Rivers on 2019/8/29.
+//  Copyright © 2019 Willow Rivers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
